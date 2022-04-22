68300fe3ba3db2e98c9fc67c16bf321622a4758d
Filip <filfer05@gmail.com>
2022-04-22 18:53:51 +0200

    navbar-published

--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -1,10 +1,10 @@
 const Navbar = () => {
     return (
         <nav className="navbar">
-            <a href="">Home</a>
-            <a href="">Sell</a>
-            <a href="">Shop</a>
-            <a href="">Cart</a>
+            <a href="#">Home</a>
+            <a href="#">Sell</a>
+            <a href="#">Shop</a>
+            <a href="#">Cart</a>
         </nav>
     )
 }
