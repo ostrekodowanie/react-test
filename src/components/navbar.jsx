cd756d8512fd6dd8b8a37b817b8e0eca9ca05f2d
Filip <filfer05@gmail.com>
2022-04-22 18:50:07 +0200

    navbar-published

---
+++ src/components/navbar.jsx
@@ -0,0 +1,12 @@
+const Navbar = () => {
+    return (
+        <nav className="navbar">
+            <a href="">Home</a>
+            <a href="">Sell</a>
+            <a href="">Shop</a>
+            <a href="">Cart</a>
+        </nav>
+    )
+}
+
+export default Navbar;
